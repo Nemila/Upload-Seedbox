8dd99d4bdd5075debd6293e778160d04941ab989
Lamine Diamoutene <pridila.2006@gmail.com>
2025-06-22 22:04:45 +0300

    add lock and filter by filename

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,34 @@ import failedService from "./failed.js";
 import hydrax from "./hydrax.js";
 import { deleteFile, getFilesRecursively } from "./manager.js";
 import wasabi from "./wasabi.js";
+import lockfile from "proper-lockfile";
+import { join } from "path";
 
-const FOLDER_PATH = process.env.sonarr_series_path;
-const EVENT_TYPE = process.env.sonarr_eventtype;
+const FOLDER_PATH = "/home/nemila/Videos/Media";
+const LOCK_FILE_PATH = join(process.cwd(), "upload-lock.lock");
 
-if (EVENT_TYPE !== "Test") {
-  (async () => {
+lockfile
+  .lock(LOCK_FILE_PATH)
+  .then(async (release) => {
     if (!FOLDER_PATH) throw new Error("No folder path");
     const files = getFilesRecursively(FOLDER_PATH);
+    const filenameRegex =
+      /^.+-(TV|MOVIE)-\d+-S\d{2}-E\d{2}(?:-\[\w{2,3}\])?(\.mkv)?$/i;
 
     for (const file of files) {
+      const filename = file.split("/").pop();
+      if (!filename) continue;
+
+      if (!filenameRegex.test(filename)) {
+        console.log("Invalid filename");
+        failedService.add({
+          message: "Invalid filename",
+          stage: "start",
+          file,
+        });
+        continue;
+      }
+
       const key = "media" + file.split(/\/media/i)[1];
       const wasabiRes = await wasabi.uploadFile(file, key);
 
@@ -40,6 +58,8 @@ if (EVENT_TYPE !== "Test") {
 
     failedService.save();
     await axios.get(`https://catoonhub.com/api/hydrax`);
-    console.log("DONE");
-  })();
-}
+    return release();
+  })
+  .catch((e) => {
+    console.error(e);
+  });
